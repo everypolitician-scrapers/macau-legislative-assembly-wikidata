b612d3648b8554916b25667860f123e8a570d038
Tony Bowden <tony@tmtm.com>
2015-11-14 12:28:49 +0000

    Ping when done

--- scraper.rb
+++ scraper.rb
@@ -27,3 +27,6 @@ WikiData.ids_from_pages('zh', members.map { |c| c[:wikiname] }).each_with_index 
   ScraperWiki.save_sqlite([:id], data)
 end
 
+require 'rest-client'
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+
