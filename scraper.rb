f2a8b3dead2e0518383afba07b139b78d1b3c8de
Tony Bowden <tony@tmtm.com>
2018-08-13 11:24:59 +0100

    Also find via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,9 @@
 
 require 'wikidata/fetcher'
 
+sparq = 'SELECT ?item WHERE { ?item p:P39/ps:P39 wd:Q28941940 }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/macau-legislative-assembly', column: 'wikiname__zh')
-EveryPolitician::Wikidata.scrape_wikidata(names: { zh: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { zh: names })
 
